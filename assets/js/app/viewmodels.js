90c5e709742b8e5cdf30da35e88bc6d3d468b14c
Jackie Ng <jumpinjackie@gmail.com>
2015-07-04 14:37:31 +1000

    Add search results. Integrate with mapguide-rest APIs for querying.

--- assets/js/app/viewmodels.js
+++ assets/js/app/viewmodels.js
@@ -2,37 +2,47 @@ function AppService(options) {
     var _this = this;
     
     var encodeOccupationQuery = function(text) {
-        return text;
+        return encodeURIComponent("\"job_name\" LIKE '%" + text + "%'");
     };
     var encodeCourseQuery = function(text) {
-        return text;
+        return encodeURIComponent("\"Course Title\" LIKE '%" + text + "%'");
     };
     
     _this.PerformSearch = function(text) {
-        var occQuery = $.getJSON(options.restUrl + "data/govhack/vet/occupations.json?filter=" + text);
-        var crsQuery = $.getJSON(options.restUrl + "data/govhack/vet/courses.json?filter=" + text);
-        return occQuery.then(crsQuery);
+        var occQuery = $.getJSON(options.restUrl + "/data/govhack/vet/occupations/.json?filter=" + encodeOccupationQuery(text));
+        var crsQuery = $.getJSON(options.restUrl + "/data/govhack/vet/courses/.json?filter=" + encodeCourseQuery(text));
+        return $.when(occQuery, crsQuery);
     };
 }
 
 function HomeViewModel() {
+    this.Title = "Home";
     this.TemplateName = "home-template";
+}
+
+function SearchResultsViewModel(occResults, crsResults) {
+    var _this = this;
+    _this.Title = "Search Resuts";
+    _this.TemplateName = "search-results-template";
+    _this.OccupationResults = ko.observableArray(occResults.features || []);
+    _this.CourseResults = ko.observableArray(crsResults.features || []);
 }
 
 function SearchViewModel(parentVm) {
     var _this = this;
     
-    _this.OccupationResults = ko.observableArray([]);
-    _this.CourseResults = ko.observableArray([]);
-    
     _this.SearchText = ko.observable("");
     _this.PerformSearch = function() {
         parentVm.IsBusy(true);
-        parentVm.Service.PerformSearch(_this.SearchText()).then(function(resp) {
-            debugger;
-            alert("Searching for: " + _this.SearchText());
+        parentVm.Service.PerformSearch(_this.SearchText()).then(function(occResult, crsResult) {
+            var occResults = occResult[0];
+            var crsResults = crsResult[0];
+            parentVm.Sidebar(new SearchResultsViewModel(occResults, crsResults));
+        }).fail(function() {
+            alert("Search Error");
         }).always(function() {
-            alert("Search error");
+            if (!$(".sidebar-container").hasClass(".slide-in"))
+                $(".navbar-toggle")[0].click();
             parentVm.IsBusy(false);
         });
     };
@@ -42,5 +52,5 @@ function AppViewModel(appService) {
     this.IsBusy = ko.observable(false);
     this.Search = new SearchViewModel(this);
     this.Sidebar = ko.observable(new HomeViewModel());
-    this.Service = new AppService();
+    this.Service = appService;
 }
