617f3dd5c0b05ec09b8ce32b494560de4784fbed
Jackie Ng <jumpinjackie@gmail.com>
2015-07-05 14:29:12 +1000

    Add parking icons and find local dataset. Also increase sidebar width and set minimum popup content width to 200px
    Also fix search to search by types with a zoom to map option for each provider result.

--- assets/js/app/viewmodels.js
+++ assets/js/app/viewmodels.js
@@ -7,11 +7,22 @@ function AppService(options) {
     var encodeCourseQuery = function(text) {
         return encodeURIComponent("\"Course Title\" LIKE '%" + text + "%'");
     };
+    var encodeProviderQuery = function (text) {
+        return encodeURIComponent("Name LIKE '%" + text + "%'")
+    }
     
-    _this.PerformSearch = function(text) {
-        var occQuery = $.getJSON(options.restUrl + "/data/govhack/vet/occupations/.json?filter=" + encodeOccupationQuery(text));
-        var crsQuery = $.getJSON(options.restUrl + "/data/govhack/vet/courses/.json?filter=" + encodeCourseQuery(text));
-        return $.when(occQuery, crsQuery);
+    _this.PerformSearch = function(text, searchType) {
+        switch (searchType)
+        {
+            case "occupations":
+                return $.getJSON(options.restUrl + "/data/govhack/vet/occupations/.json?filter=" + encodeOccupationQuery(text));
+            case "courses":
+                return $.getJSON(options.restUrl + "/data/govhack/vet/courses/.json?filter=" + encodeCourseQuery(text));
+            case "providers":
+                return $.getJSON(options.restUrl + "/data/govhack/vet/providers/.json?filter=" + encodeProviderQuery(text));
+            default:
+                throw "Unsupported search type";
+        }
     };
 }
 
@@ -20,7 +31,7 @@ function HomeViewModel() {
     this.TemplateName = "home-template";
 }
 
-function SearchResultsViewModel(occResults, crsResults) {
+function SearchResultsViewModel(results) {
     var _this = this;
     _this.Title = "Search Resuts";
     _this.TemplateName = "search-results-template";
@@ -36,24 +47,51 @@ function CourseOccupationListViewModel() {
     
 }
 
-function OccupationListViewModel() {
+function CourseListViewModel(results) {
+    var _this = this;
+    _this.Title = "Course Search Results (" + results.features.length + ")";
+    _this.TemplateName = "course-list-template";
     
+    _this.Results = ko.observableArray(results.features);
 }
 
-function ProviderListViewModel() {
+function OccupationListViewModel(results) {
+    var _this = this;
+    _this.Title = "Occupation Search Results (" + results.features.length + ")";
+    _this.TemplateName = "occupation-list-template";
     
+    _this.Results = ko.observableArray(results.features);
+}
+
+function ProviderListViewModel(results) {
+    var _this = this;
+    _this.Title = "Provider Search Results (" + results.features.length + ")";
+    _this.TemplateName = "provider-list-template";
+    
+    _this.Results = ko.observableArray(results.features);
 }
 
 function SearchViewModel(parentVm) {
     var _this = this;
     
+    _this.SearchType = ko.observable("courses");
+    _this.SearchTypes = ["courses", "providers", "occupations"];
     _this.SearchText = ko.observable("");
     _this.PerformSearch = function() {
         parentVm.IsBusy(true);
-        parentVm.Service.PerformSearch(_this.SearchText()).then(function(occResult, crsResult) {
-            var occResults = occResult[0];
-            var crsResults = crsResult[0];
-            parentVm.Sidebar(new SearchResultsViewModel(occResults, crsResults));
+        var stype = _this.SearchType();
+        parentVm.Service.PerformSearch(_this.SearchText(), stype).then(function(result) {
+            switch (stype) {
+                case "courses":
+                    parentVm.Sidebar(new CourseListViewModel(result));
+                    break;
+                case "occupations":
+                    parentVm.Sidebar(new OccupationListViewModel(result));
+                    break;
+                case "providers":
+                    parentVm.Sidebar(new ProviderListViewModel(result));
+                    break;
+            }
         }).fail(function() {
             alert("Search Error");
         }).always(function() {
@@ -63,7 +101,7 @@ function SearchViewModel(parentVm) {
     };
 }
 
-function AppViewModel(appService) {
+function AppViewModel(map, appService) {
     this.IsBusy = ko.observable(false);
     this.Search = new SearchViewModel(this);
     this.Sidebar = ko.observable(new HomeViewModel());
@@ -75,5 +113,15 @@ function AppViewModel(appService) {
     this.HideSidebar = function() {
         if ($(".sidebar-container").is(".slide-in"))
             $(".navbar-toggle")[0].click();
+    };
+    this.ZoomToLonLat = function(lon, lat) {
+        //HACK: It's prematurely called as part of data-binding, but it should
+        //be busy at this point so deny any zoom requests
+        if (this.IsBusy())
+            return;
+            
+        var view = map.getView();
+        view.setCenter(ol.proj.transform([lon, lat], "EPSG:4326", "EPSG:3857"));
+        view.setZoom(17);
     };
 }
